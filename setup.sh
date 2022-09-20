db9bd871ac007adaa2718f67d1f61d965c153270
Chien Tran <70304839+chientrm@users.noreply.github.com>
2022-09-21 06:31:34 +0700

    f

--- setup.sh
+++ setup.sh
@@ -28,3 +28,7 @@ $HOME/steamcmd/steamcmd.sh\
     +login anonymous\
     +app_update 343050 validate\
     +quit
+
+curl -L --output cloudflared.deb https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb
+sudo dpkg -i cloudflared.deb
+rm cloudflared.deb
